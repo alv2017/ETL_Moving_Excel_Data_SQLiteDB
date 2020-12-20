c2ee898f308e4f75b31e0b8e63583b8b924f342b
alv2017 <v.alishauskaite@gmail.com>
2020-12-20 21:26:32 +0200

    settings.py has been updated: QUERIES dictionary containing sql queries has been added. Testing directory settings has been added.

--- settings.py
+++ settings.py
@@ -6,7 +6,9 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 ## Data
 DATA_DIR_NAME = "data"
+TEST_DIR_NAME = "testing"
 DATA_DIR = os.path.join(ROOT_DIR, DATA_DIR_NAME)
+TEST_DATA_DIR = os.path.join(ROOT_DIR, DATA_DIR_NAME, TEST_DIR_NAME)
 
 ## DB
 DB_DIR_NAME = "db"
@@ -28,7 +30,18 @@ APP_LOG_FILE = os.path.join(ROOT_DIR, LOG_DIR_NAME, APP_LOG_FILE_NAME)
 READ_LOG_FILE = os.path.join(ROOT_DIR, LOG_DIR_NAME, READ_LOG_FILE_NAME)
 INSERT_LOG_FILE = os.path.join(ROOT_DIR, LOG_DIR_NAME, INSERT_LOG_FILE_NAME)
 
-
-
-
-
+QUERIES = {
+    "create_prices_table": """CREATE TABLE IF NOT exists {0}(
+                                price_time TEXT NOT NULL,
+                                region TEXT NOT NULL,
+                                price REAL NOT NULL,
+                                PRIMARY KEY (price_time, region)                               
+                            ) """.format(TABLE_NAME),
+    
+    "insert_price": """INSERT INTO {0} (price_time, region, price)
+                       VALUES (?, ?, ?) 
+                    """.format(TABLE_NAME),
+                    
+    "delete_from_prices": """DELETE FROM {}""".format(TABLE_NAME)
+    
+    }
